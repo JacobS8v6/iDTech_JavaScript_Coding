8e3fb8f8b43b89a927f6b77e9c654c3bfeb2cf96
Jacob Smith <jblaster05@gmail.com>
2022-07-08 00:34:26 -0700

    added story_time.js

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,33 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let intro = "Welcome to humanity, 40,000 years in the future.";
+var name = ' Roboute Guilliman';
+let title = 'the Orks next target';
+let worldName = 'Ultramar';
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+
+let year = 42022;
+let orkNumber = 1000000000;
+let spaceMarineNumber = 10000;
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let items = ['Flaming Power Sword', 'Iron Halo', 'sick drip' ];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let chanceOfDefeat = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(intro +" where in the grim darkness of the far future, there is only war." + name +" and his army of Space Marines are the final line of defense against the green tide of aliens know as Orks.");
+console.log(title + " is " + worldName + ", Guilliman's home planet.");
+console.log("although the Spcae Marines are outnumbered " + spaceMarineNumber + " to " + orkNumber + " Space marines still have a chance of victory for each soldier is basically equivalent to Captain America, while the Orks are untrained bros just trying to have a good fight.");
+console.log("as the Orks 'land' on the planet in their giant rock, Robot, I mean Roboute and the other marines begin to clash with the Orks." );
+console.log("With his " + items[0] + "he cleaved away on several Orks at once, making his way through the ranks until he encountered an Ork Warboss clad in giant yellow rocket launchers.");
+console.log(" 'FWOOSH' at once, all of the missiles were dumped on Guilliman at 150mph. Protected by the force field from his " + items[1] + ", Sillyman- I mean, Guilliman emerged from the smoke mostly unscathed and his " + items[2] + " remained intact." );
+console.log("the great Ork sprinted at Guilliman, revealing his true height to be a whole head and shoulders taller than Guilliman, and sent him flying into a nearby boulder. ");
+console.log("'Did ya see that boys!? I sent 'im flying! And the boss says Beaky Boys are a threat.' Exclaimed the warboss to his followers.");
+console.log("Guilliman replies, 'It's" + chanceOfDefeat + "that you may seem to have the upper hand on paper... But in reality, you're fighting a 10,000 year old Primarch and his finest warriors on their home planet.' ");
+console.log("Guilliman advances on the Warboss, swinging his Power Sword at it's face, only to be met with a previously hidden axe. the two leaders meet blow after blow until the Ork goes for a surprise headbutt which allows guilliman to slice straight through the Ork. The Warboss' followers astonished that their boss lost, pick up their weapons and charge at Guilliman in vengeance. And so, the war continues and the prohphecy lives on: in the grim darkness of the far future, there is only war."); 
